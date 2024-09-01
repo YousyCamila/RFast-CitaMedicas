00f51cf84145ffe032c1cf7e878ed30ce65825c9
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-08-31 21:41:59 -0500

    feat(back-end): Add citaRoutes.js

---
+++ backend/routes/citaRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const citasController = require('../controllers/citas');
+
+// Crear una nueva cita
+router.post('/', citasController.createCita);
+
+// Obtener todas las citas
+router.get('/', citasController.getCitas);
+
+// Obtener una cita por ID
+router.get('/:id', citasController.getCitaById);
+
+// Actualizar una cita por ID
+router.put('/:id', citasController.updateCita);
+
+// Eliminar una cita por ID
+router.delete('/:id', citasController.deleteCita);
+
+module.exports = router;
