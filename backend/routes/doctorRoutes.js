73bd056037268fbd561b106a866ad9feb5300e82
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-08-31 21:40:47 -0500

    feat(back-end): Add doctorRoutes.js

---
+++ backend/routes/doctorRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const doctorsController = require('../controllers/doctors');
+
+// Rutas para doctores
+router.post('/', doctorsController.createDoctor);
+router.get('/', doctorsController.getDoctors);
+router.get('/:email', doctorsController.getDoctorByEmail);
+router.put('/:email', doctorsController.updateDoctor);
+router.delete('/:email', doctorsController.deleteDoctor);
+
+module.exports = router;
