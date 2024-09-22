da8b9218f3edf929c9a8980d36c4eb06ea7e41d4
YousyCamila <166034381+YousyCamila@users.noreply.github.com>
2024-09-21 19:49:48 -0500

    feat(backend): Add Mongo atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,15 +4,18 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/medicalappointmentsdb');
-    console.log('Conectado a MongoDB Atlas');
+    // Usa la URL de conexión de la variable de entorno
+    const dbURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/medicalappointmentsdb';
+    await mongoose.connect(dbURI);
+    console.log('Conectado a MongoDB');
   } catch (error) {
-    console.error('Error al conectar a MongoDB Atlas', error);
+    console.error('Error al conectar a MongoDB', error);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
 
 
 
